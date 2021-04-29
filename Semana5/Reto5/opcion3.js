0e3b372ee3e5029f45b6d0402db2213cd08bdab4
DiegoSanZar <81200983+DiegoSanZar@users.noreply.github.com>
2021-04-29 18:10:51 -0500

    Create opcion3.js

---
+++ Semana5/Reto5/opcion3.js
@@ -0,0 +1,10 @@
+Numcita = +prompt("Ingrese el numero de cita")
+pricosto = 200
+segcosto = 150
+tercosto = 100
+rescost = 50
+
+if( Numcita>=1 && Numcita<=3){
+    console.log("El costo de la cita es de 200 dolares")
+    console.log("El tratamiento lleva costando" + (Numcita*pricosto) )
+}
